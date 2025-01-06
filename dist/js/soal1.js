16c0c9321325f56eab0b29e3cfe1952dd7226ece
Agus Prasetyo <agusprasetyo1889@gmail.com>
2025-01-07 00:31:53 +0700

    finish soal1

---
+++ dist/js/soal1.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {    
+    $('#number_of_disk').keypress(e => {
+        var charCode = (e.which) ? e.which : e.keyCode;
+        // Check if the pressed key is an alphabet (A-Z or a-z)
+        if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+            e.preventDefault(); // Prevent the character from being entered
+            // Optionally provide feedback to the user
+            alert("Input hanya boleh angka");
+        }
+    })
+
+    $('#number_of_disk').on('input', function() {
+        let value = $(this).val()
+
+        // Konfigurasi min, max, default value
+        if(value === "") {
+            $(this).val('');
+        } else if (parseInt(value) < 1) {
+            alert("Minimal input 1")
+            $(this).val(1);
+        } else if (parseInt(value) > 8) {
+            alert("Maksimal input 8")
+            $(this).val(8);
+        } else {
+            $(this).val(parseInt(value)); // Ensure the value is integer
+        }
+    })
+
+    $('#raid_type').mousedown(function() {
+
+        if ($('#number_of_disk').val() == '') {
+            alert('Number of disk harap diisi')
+            return
+        } else if ($('#size_per_disk').val() == null) {
+            alert('Size per disk belum dipilih')
+            return
+        }
+
+        let number_of_disk = checkInputNumberOfDisk($('#number_of_disk').val())
+        
+        for (const value of number_of_disk) {
+            $(`#raid_type option[value='${value}']`).prop('disabled', false)
+        }
+    })
+
+    $('#raid_type').change(function() {
+        
+        $('.raid_type_output').html($(this).val())
+
+        $('#output_capacity').html(outputCapacityCount($(this).val()))
+        $('#output_fault_tolerance').html(outputFaultToleranceCount($(this).val()))
+    })
+})
+
+// check input rule
+function checkInputNumberOfDisk(number_of_disk) {
+    // Make all select option default disabled
+    $("#raid_type option").prop("disabled", true);
+    $("#raid_type option:eq(1)").prop("disabled", false);
+
+    // to fill array temporary
+    let temp = []
+
+    if (number_of_disk >= 2 && number_of_disk <= 8) { //rule raid-1
+        temp.push('RAID-1')
+    }
+
+    if (number_of_disk >= 3 && number_of_disk <= 8) { //rule raid-5
+        temp.push('RAID-5')
+    }
+
+    if ([4,6,8].includes(parseInt(number_of_disk))) { //rule raid-10
+        temp.push('RAID-10')
+    }
+
+    return temp
+}
+
+// Digunakan untuk mennghitung capacity
+function outputCapacityCount(raid_type) {
+    let fix_number_of_disk = $('#number_of_disk').val() == '' ? 1 : $('#number_of_disk').val()
+    let size_per_disk = $('#size_per_disk').val() == '' ? 1 : $('#size_per_disk').val()
+
+    if (raid_type == 'RAID-0') {
+        return parseInt(fix_number_of_disk) * size_per_disk
+    } else if (raid_type == 'RAID-1') {
+        return size_per_disk
+    } else if (raid_type == 'RAID-5') {
+        return (parseInt(fix_number_of_disk) - 1) * parseInt(size_per_disk)
+    } else if (raid_type == 'RAID-10') {
+        return (parseInt(fix_number_of_disk) / 2) * size_per_disk
+    }
+}
+
+// digunakan untuk menghitung fault tolerance
+function outputFaultToleranceCount(raid_type) {
+    if (raid_type == 'RAID-0') {
+        return 0
+    } else if (raid_type == 'RAID-1') {
+        let fix_number_of_disk = $('#number_of_disk').val() == '' ? 1 : $('#number_of_disk').val()
+
+        return parseInt(fix_number_of_disk) - 1
+    } else if (raid_type == 'RAID-5' || raid_type == 'RAID-10') {
+        return 1
+    }
+
+}
--- resource/soal/soal1.php
+++ resource/soal/soal1.php
@@ -1,18 +1,50 @@
 <?php include('../layouts/header.php') ?>
 
     <div class="row justify-content-center" style="margin-top: 100px;">
-        <div class="col-8">    
+        <div class="col-4">
+            <h1 class="fs-4">Soal 1</h1>    
             <div class="card">
                 <div class="card-body">
-                    <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-                        <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
-                        <label class="btn btn-outline-primary" for="btncheck1">Checkbox 1</label>
+                    <div class="mb-3">
+                        <label for="number_of_disk" class="form-label">Input Number of Disk</label>
+                        <input type="text" class="form-control" name="number_of_disk" id="number_of_disk">
+                    </div>
+                    <div class="mb-3">
+                        <label for="size_per_disk" class="form-label">Input Size per Disk</label>
 
-                        <input type="checkbox" class="btn-check" id="btncheck2" autocomplete="off">
-                        <label class="btn btn-outline-primary" for="btncheck2">Checkbox 2</label>
+                        <select class="form-select" style="cursor: pointer;" name="size_per_disk" id="size_per_disk">
+                            <option selected disabled>Select Size per Disk</option>
+                            <option value="300">300 GB</option>
+                            <option value="500">500 GB</option>
+                            <option value="1000">1 TB</option>
+                            <option value="2000">2 TB</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="raid_type" class="form-label">Input RAID Type</label>
 
-                        <input type="checkbox" class="btn-check" id="btncheck3" autocomplete="off">
-                        <label class="btn btn-outline-primary" for="btncheck3">Checkbox 3</label>
+                        <select class="form-select" style="cursor: pointer;" name="raid_type" id="raid_type">
+                            <option selected disabled>Select Size per Disk</option>
+                            <option value="RAID-0">RAID-0</option>
+                            <option value="RAID-1" disabled>RAID-1</option>
+                            <option value="RAID-5" disabled>RAID-5</option>
+                            <option value="RAID-10" disabled>RAID-10</option>
+                        </select>
+                    </div>
+                    
+                    <h2 class="fs-5">Output</h2>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <h3 class="fs-6 fw-normal">Output Capacity</h3>
+                            <div>Tipe : <span class="raid_type_output">-</span></div> 
+                            <h4 id="output_capacity">-</h4>
+                        </div>
+                        <div class="col-6">
+                            <h3 class="fs-6 fw-normal">Output Fault Tolerance</h3>
+                            <div>Tipe : <span class="raid_type_output">-</span></div>
+                            <h4 id="output_fault_tolerance">-</h4>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -21,8 +53,4 @@
 
 <?php include('../layouts/footer.php') ?>
 
-<script>
-    $(document).ready(function() {    
-        $('#kocak').append('<h2>yahahhha</h2>')
-    })
-</script>
+<script src="../../dist/js/soal1.js"></script>
